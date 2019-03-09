da5fda4ad4fb3e165c6ce09e79acd814c4c40e98
joke111 <alta.second@gmail.com>
2019-03-09 18:02:03 +0900

    leetcode - no.560

--- leetcode/src/solved/560_SubarraySumEqualsK.java
+++ leetcode/src/solved/560_SubarraySumEqualsK.java
@@ -23,3 +23,26 @@ class Solution {
 		return count;
     }
 }
+
+//second answer(cheated)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+		Map<Integer, Integer> sums = new HashMap<>();
+		
+		int count = 0;
+		int current = 0;
+		for (int i = 0; i < nums.length; i++) {
+			current += nums[i];
+			if (current == k) {
+				count++;
+			}
+			count += sums.getOrDefault(current - k, 0);
+			if (sums.containsKey(current)) {
+				sums.put(current, sums.get(current) + 1);
+			} else {
+				sums.put(current, 1);
+			}
+		}
+		return count;
+	}
+}
