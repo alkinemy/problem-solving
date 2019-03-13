9d9ff335af7cd865e781660e0ddc2b07ca5f8fd1
joke111 <alta.second@gmail.com>
2019-03-14 01:36:41 +0900

    leetcode - no.32

--- leetcode/src/solved/32_LongestValidParentheses.java
+++ leetcode/src/solved/32_LongestValidParentheses.java
@@ -2,50 +2,50 @@
  * https://leetcode.com/problems/longest-valid-parentheses/
  */
 
+//first answer
 class Solution {
     public int longestValidParentheses(String s) {
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
-
-		Stack<Integer> lengths = new Stack<>();
-		Stack<Character> stack = new Stack<>();
+		// ( = -1
+		int result = 0;
+		Stack<Integer> stack = new Stack<>();
 		for(int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
 			if (c == '(') {
-				stack.push('(');
-			} else if (c == ')') {
-				if (stack.isEmpty()) {
-					continue;
-				}
+				stack.push(-1);
+			} else { // ')'
 				int length = 0;
+				boolean found = false;
 				while(!stack.isEmpty()) {
-					char popped = stack.pop();
-					if (popped == '-') {
-						length += lengths.pop();
-					} else {
+					int popped = stack.pop();
+					if (popped == -1) {
 						length += 2;
+						found = true;
 						break;
-					}
-				}
-				while(!stack.isEmpty()) {
-					char popped = stack.pop();
-					if (popped == '-') {
-						length += lengths.pop();
 					} else {
-						stack.push(popped);
-						break;
+						length += popped;
 					}
 				}
-				lengths.push(length);
-				stack.push('-');
+				if (found) {
+					stack.push(length);
+				} else {
+					result = Math.max(result, length);
+				}
 			}
 		}
-
-		int result = 0;
-		while(!lengths.isEmpty()) {
-			result = Math.max(result, lengths.pop());
-		}
-		return result;
+		
+        int current = 0;
+        while(!stack.isEmpty()) {
+            int length = stack.pop();
+            if (length == -1) {
+                result = Math.max(result, current);
+                current = 0;
+            } else {
+                current += length;
+            }
+        }
+        return Math.max(result, current);
     }
 }
