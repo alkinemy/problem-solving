8235df10df7c6a45b4ff4efe7cac179bcd07046a
joke111 <alta.second@gmail.com>
2019-03-18 22:37:39 +0900

    leetcode - no.54

--- leetcode/src/solved/54_SpiralMatrix.java
+++ leetcode/src/solved/54_SpiralMatrix.java
@@ -2,38 +2,47 @@
  * https://leetcode.com/problems/spiral-matrix/
  */
 
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-			return Collections.emptyList();
-		}
-		
-		int shortLength = Math.min(matrix.length, matrix[0].length);
-		List<Integer> result = new ArrayList<>();
-		for (int i = 0; i <= shortLength / 2; i++) {
-			//left -> right
-			
-			for(int c = i; c < matrix[i].length - i; c++) {
-				result.add(matrix[i][c]);
-			}
-			//up -> down
-			for(int r = i + 1; r < matrix.length - i; r++) {
-				result.add(matrix[r][matrix[i].length - 1 - i]);
-			}
-			//right -> left
-			if (matrix.length - 1 - i != i) {
-				for (int c = matrix[i].length - 2 - i; c >= i; c--) {
-					result.add(matrix[matrix.length - 1 - i][c]);
-				}
-			}
-			//down -> up
-			if (matrix[i].length - 1 - i != i) {
-				for (int r = matrix.length - 2 - i; r > i; r--) {
-					result.add(matrix[r][i]);
-				}
-			}
-		}
-		return result;
-    }
+            return Collections.emptyList();
+        }
+    
+        int shortLength = Math.min(matrix.length, matrix[0].length);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < shortLength / 2; i++) {
+            //left -> right
+            for(int c = i; c < matrix[i].length - i; c++) {
+                result.add(matrix[i][c]);
+            }
+            //up -> down
+            for(int r = i + 1; r < matrix.length - i; r++) {
+                result.add(matrix[r][matrix[i].length - 1 - i]);
+            }   
+            //right -> left
+            for (int c = matrix[i].length - 2 - i; c >= i; c--) {
+                result.add(matrix[matrix.length - 1 - i][c]);
+            }   
+            //down -> up
+            for (int r = matrix.length - 2 - i; r > i; r--) {
+                result.add(matrix[r][i]);
+            }   
+        }   
+        
+        if (shortLength % 2 == 1) {
+            int i = shortLength / 2;
+            if (matrix.length == shortLength) { //row
+                for (int c = i; c < matrix[i].length - i; c++){
+                    result.add(matrix[i][c]);
+                }
+            } else { //column
+                for (int r = i; r < matrix.length - i; r++) {
+                    result.add(matrix[r][i]);
+                }
+            }   
+        } 
+            
+        return result;
+    }   
 }
+
