26a817a0bd6cd3ae5a19651fc1c8c650e0f8d1d3
joke111 <alta.second@gmail.com>
2019-03-19 13:30:35 +0900

    leetcode - no.17(solution)

--- leetcode/src/solved/17. Letter Combinations of a Phone Number.java
+++ leetcode/src/solved/17. Letter Combinations of a Phone Number.java
@@ -2,6 +2,7 @@
  * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  */
 
+//first solution
 class Solution {
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) {
@@ -41,4 +42,43 @@ class Solution {
         }
         return strs;
     }
+}
+
+
+
+
+//backtracking(solution)
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return Collections.emptyList();
+        }
+
+        Map<Character, char[]> mappings = new HashMap<>();
+        mappings.put('2', new char[] {'a', 'b', 'c'});
+        mappings.put('3', new char[] {'d', 'e', 'f'});
+        mappings.put('4', new char[] {'g', 'h', 'i'});
+        mappings.put('5', new char[] {'j', 'k', 'l'});
+        mappings.put('6', new char[] {'m', 'n', 'o'});
+        mappings.put('7', new char[] {'p', 'q', 'r', 's'});
+        mappings.put('8', new char[] {'t', 'u', 'v'});
+        mappings.put('9', new char[] {'w', 'x', 'y', 'z'});
+
+		List<String> result = new ArrayList<>();
+		backtrack(digits, mappings, "", result);
+		return result;
+	}
+
+	void backtrack(String digits, Map<Character, char[]> mappings, String combined, List<String> result) {
+		if (digits.length() == 0) {
+			result.add(combined);
+			return;
+		}
+
+		char[] mapping = mappings.get(digits.charAt(0));
+		String nextDigits = digits.substring(1, digits.length());
+		for (char c : mapping) {
+			backtrack(nextDigits, mappings, combined + c, result);
+		}
+	}
 }
