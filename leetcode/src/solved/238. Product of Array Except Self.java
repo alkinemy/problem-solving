94ab8a25baab07415ce30d2e3576f17be0e3b041
joke111 <alta.second@gmail.com>
2019-03-24 21:40:24 +0900

    leetcode - no.238

--- leetcode/src/solved/238. Product of Array Except Self.java
+++ leetcode/src/solved/238. Product of Array Except Self.java
@@ -2,6 +2,7 @@
  * https://leetcode.com/problems/product-of-array-except-self/
  */
 
+//first answer
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 		if (nums.length == 0) {
@@ -23,5 +24,30 @@ class Solution {
 			result[i] = left * right;
 		}
 		return result;
+    }
+}
+
+
+//second answer(little bit fast)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums.length == 0) {
+            return new int[0];
+        }
+        int[] fromLeft = new int[nums.length];
+        int[] fromRight = new int[nums.length];
+
+        fromRight[nums.length - 1] = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            fromRight[i] = fromRight[i + 1] * nums[i];
+        }
+        int left = 1;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int right = (i + 1) < nums.length ? fromRight[i + 1] : 1;
+            result[i] = left * right;
+            left *= nums[i];
+        }
+        return result;
     }
 }
