c82ff951cf81df750a93e5b062597504e0014cc1
joke111 <alta.second@gmail.com>
2019-03-25 12:50:04 +0900

    leetcode - no.31(solution)

--- leetcode/src/solved/31. Next Permutation.java
+++ leetcode/src/solved/31. Next Permutation.java
@@ -2,39 +2,87 @@
  * https://leetcode.com/problems/next-permutation/
  */
 
+//first answer
 class Solution {
     public void nextPermutation(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return;
-		}
+        if (nums == null || nums.length == 0) {
+            return;
+        }
 
-		int from = -1;
-		int to = nums.length - 1;
-		for (int i = nums.length - 1; i > 0; i--) {
-			if (nums[i - 1] < nums[i]) {
-				from = i;
-				break;
-			}
-		}
-		if (from == -1) {
-			Arrays.sort(nums);
-			return nums;
-		}
+        int from = -1;
+        int to = nums.length - 1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                from = i - 1;
+                break;
+            }
+        }
+        if (from == -1) {
+            Arrays.sort(nums);
+            return;
+        }
 
-		for (int i = nums.length - 1; i > from; i--) {
-			if (nums[from] < nums[i]) {
-				to = i;
-				break;
-			}
-		}
+        for (int i = nums.length - 1; i > from; i--) {
+            if (nums[from] < nums[i]) {
+                to = i;
+                break;
+            }
+        }
 
-		swap(nums, from, to);
-		return Arrays.sort(nums, from + 1, nums.length);
+        swap(nums, from, to);
+        Arrays.sort(nums, from + 1, nums.length);
     }
 
-	void swap(int[] nums, int from, int to) {
-		int temp = nums[from];
-		nums[from] = nums[to];
-		nums[to] = temp;
+    void swap(int[] nums, int from, int to) {
+        int temp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = temp;
+    }
+}
+
+
+//solution(use reversing intead of sort)
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int from = -1;
+        int to = nums.length - 1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                from = i - 1;
+                break;
+            }
+        }
+        if (from == -1) {
+			reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        for (int i = nums.length - 1; i > from; i--) {
+            if (nums[from] < nums[i]) {
+                to = i;
+                break;
+            }
+        }
+
+        swap(nums, from, to);
+		reverse(nums, from + 1, nums.length - 1);
+    }
+
+    void swap(int[] nums, int from, int to) {
+        int temp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = temp;
+    }
+
+	void reverse(int[] nums, int from, int to) {
+		while(from < to) {
+			swap(nums, from, to);
+			from++;
+			to--;
+		}
 	}
 }
