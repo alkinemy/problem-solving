92ea8e0380a72ceddb9e53cb883e372d2ab6d403
joke111 <alta.second@gmail.com>
2019-03-25 13:09:38 +0900

    leetcode - no.239(another way)

--- leetcode/src/solved/289. Game of Life.java
+++ leetcode/src/solved/289. Game of Life.java
@@ -45,7 +45,7 @@ class Solution {
 
 
 
-//second answer(in-place) space: O(M)
+//second answer(in-place?) space: O(N)
 class Solution {
     public void gameOfLife(int[][] board) {
 		if (board == null || board.length == 0 || board[0].length == 0) {
@@ -106,4 +106,75 @@ class Solution {
 		return count;
 	}
 }
+
+
+//solution? use bit, space: O(1)
+class Solution {
+    public void gameOfLife(int[][] board) {
+		if (board == null || board.length == 0 || board[0].length == 0) {
+			return;
+		}
+
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[r].length; c++) {
+				int lives = countLiveNeighbors(board, r, c);
+				if (board[r][c] == 1 && lives >= 2 && lives <= 3) {
+					board[r][c] = 3;
+				}
+				if (board[r][c] == 0 && lives == 3) {
+					board[r][c] = 2;
+				}
+			}
+		}
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[r].length; c++) {
+				board[r][c] >>= 1;
+			}
+		}
+	}
+
+	int countLiveNeighbors(int[][] board, int r, int c) {
+		//좌상/상/우상/우/우하/하/좌하/좌
+		int[] rows =    {-1, -1, -1, 0, 1, 1,  1,  0};
+		int[] columns = {-1,  0,  1, 1, 1, 0, -1, -1};
+
+		int count = 0;
+		for (int i = 0; i < 8; i++) {
+			int row = r + rows[i];
+			int column = c + columns[i];
+			if (row >= 0 && row < board.length && column >= 0 && column < board[row].length) {
+				count += board[row][column] & 1;
+			}
+		}
+		return count;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
