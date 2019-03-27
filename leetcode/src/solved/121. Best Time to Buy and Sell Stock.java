5e97ac8e58eb7c0722c28c9028427c8adb2ac289
joke111 <alta.second@gmail.com>
2019-03-27 12:12:42 +0900

    leetcode - no.121(solution)

--- leetcode/src/solved/121. Best Time to Buy and Sell Stock.java
+++ leetcode/src/solved/121. Best Time to Buy and Sell Stock.java
@@ -32,3 +32,24 @@ class Solution {
 		return profit;
     }
 }
+
+
+
+//solution
+class Solution {
+    public int maxProfit(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+
+		int min = Integer.MAX_VALUE;
+		int profit = 0;
+		for (int i = 0; i < prices.length; i++) {
+			if (min > prices[i]) {
+				min = prices[i];
+			}
+			profit = Math.max(profit, prices[i] - min);
+		}
+		return profit;
+    }
+}
