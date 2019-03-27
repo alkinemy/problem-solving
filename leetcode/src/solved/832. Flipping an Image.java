56b251a6691fe6278b4a036998841a6024716b51
joke111 <alta.second@gmail.com>
2019-03-27 12:38:04 +0900

    leetcode - no.832(solution)

--- leetcode/src/solved/832. Flipping an Image.java
+++ leetcode/src/solved/832. Flipping an Image.java
@@ -2,6 +2,7 @@
  * https://leetcode.com/problems/flipping-an-image/
  */
 
+//answer, time: O(MN), space: O(MN)
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
 		if (A == null || A.length == 0 || A[0] == null || A[0].length == 0) {
@@ -16,5 +17,24 @@ class Solution {
 			}
 		}
 		return result;
+    }
+}
+
+//solution, time: O(MN), space: O(1)
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        if (A == null || A.length == 0 || A[0] == null || A[0].length == 0) {
+            return null;
+        }
+
+        for (int r = 0; r < A.length; r++) {
+            int columnLength = A[r].length;
+            for (int c = 0; c < (columnLength + 1) / 2; c++) {
+                int temp = A[r][c] ^ 1;
+                A[r][c] = A[r][columnLength - c - 1] ^ 1;
+                A[r][columnLength - c - 1] = temp;
+            }
+        }
+        return A;
     }
 }
