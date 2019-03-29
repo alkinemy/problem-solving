8bbc173f3c7f6b632eb5b5b896469356357f9d3f
joke111 <alta.second@gmail.com>
2019-03-30 00:16:04 +0900

    leetcode - no.47(solution)

--- leetcode/src/solved/47. Permutations II.java
+++ leetcode/src/solved/47. Permutations II.java
@@ -41,3 +41,107 @@ class Solution {
 		return result;
 	}
 }
+
+
+//book solution
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return Collections.emptyList();
+		}
+		
+		Map<Integer, Integer> counts = new HashMap<>();
+		for (int num : nums) {
+			int count = counts.getOrDefault(num, 0) + 1;
+			counts.put(num, count);
+		}
+		
+		List<List<Integer>> result = new ArrayList<>();
+		computePermutations(counts, nums.length, new ArrayList<>(), result);
+		return result;
+	}
+
+	void computePermutations(Map<Integer, Integer> counts, int remaining, List<Integer> current, List<List<Integer>> result) {
+		if (remaining == 0) {
+			result.add(new ArrayList<>(current));
+			return;
+		}
+
+		for (int num : counts.keySet()) {
+			int count = counts.get(num);
+			if (count > 0) {
+				counts.put(num, count - 1);
+				int currentLength = current.size();
+				current.add(num);
+				computePermutations(counts, remaining - 1, current, result);
+				current.remove(currentLength);
+				counts.put(num, count);
+			}
+		}
+	}
+}
+
+
+//faster solution
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return Collections.emptyList();
+		}
+
+		Arrays.sort(nums);
+		boolean[] visited = new boolean[nums.length];
+		List<List<Integer>> result = new ArrayList<>();
+		getPermutations(nums, new ArrayList<>(), visited, result);
+		return result;
+	}
+
+	void getPermutations(int[] nums, List<Integer> current, boolean[] visited, List<List<Integer>> result) {
+		if (current.size() == nums.length) {
+			result.add(new ArrayList<>(current));
+			return;
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			if (visited[i]) {
+				continue;
+			}
+			if (i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) {
+				continue;
+			}
+
+			visited[i] = true;
+			int value = nums[i];
+			current.add(value);
+			getPermutations(nums, current, visited, result);
+			current.remove(current.size() - 1);
+			visited[i] = false;
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
