d5092d0ee2871124918993160c79f7179e0c7843
joke111 <alta.second@gmail.com>
2019-03-31 18:08:34 +0900

    indent

--- leetcode/src/solved/17. Letter Combinations of a Phone Number.java
+++ leetcode/src/solved/17. Letter Combinations of a Phone Number.java
@@ -4,44 +4,44 @@
 
 //first solution
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.length() == 0) {
-            return Collections.emptyList();
-        }
+	public List<String> letterCombinations(String digits) {
+		if (digits == null || digits.length() == 0) {
+			return Collections.emptyList();
+		}
 
-        Map<Character, char[]> assigned = new HashMap<>();
-        assigned.put('2', new char[] {'a', 'b', 'c'});
-        assigned.put('3', new char[] {'d', 'e', 'f'});
-        assigned.put('4', new char[] {'g', 'h', 'i'});
-        assigned.put('5', new char[] {'j', 'k', 'l'});
-        assigned.put('6', new char[] {'m', 'n', 'o'});
-        assigned.put('7', new char[] {'p', 'q', 'r', 's'});
-        assigned.put('8', new char[] {'t', 'u', 'v'});
-        assigned.put('9', new char[] {'w', 'x', 'y', 'z'});
+		Map<Character, char[]> assigned = new HashMap<>();
+		assigned.put('2', new char[] {'a', 'b', 'c'});
+		assigned.put('3', new char[] {'d', 'e', 'f'});
+		assigned.put('4', new char[] {'g', 'h', 'i'});
+		assigned.put('5', new char[] {'j', 'k', 'l'});
+		assigned.put('6', new char[] {'m', 'n', 'o'});
+		assigned.put('7', new char[] {'p', 'q', 'r', 's'});
+		assigned.put('8', new char[] {'t', 'u', 'v'});
+		assigned.put('9', new char[] {'w', 'x', 'y', 'z'});
 
-        int rows = 1;
-        for (char c : digits.toCharArray()) {
-            rows *= assigned.get(c).length;
-        }
-        int columns = digits.length();
-        char[][] result = new char[rows][columns];
-        int repeat = 1;
-        for (int c = digits.length() - 1; c >= 0; c--) {
-            char current = digits.charAt(c);
-            char[] available = assigned.get(current);
-            for (int r = 0; r < rows; r++) {
-                int index = ((r / repeat) % available.length);
-                result[r][c] = available[index];
-            }
-            repeat *= available.length;
-        }
+		int rows = 1;
+		for (char c : digits.toCharArray()) {
+			rows *= assigned.get(c).length;
+		}
+		int columns = digits.length();
+		char[][] result = new char[rows][columns];
+		int repeat = 1;
+		for (int c = digits.length() - 1; c >= 0; c--) {
+			char current = digits.charAt(c);
+			char[] available = assigned.get(current);
+			for (int r = 0; r < rows; r++) {
+				int index = ((r / repeat) % available.length);
+				result[r][c] = available[index];
+			}
+			repeat *= available.length;
+		}
 
-        List<String> strs = new ArrayList<>();
-        for (char[] array : result) {
-            strs.add(new String(array));
-        }
-        return strs;
-    }
+		List<String> strs = new ArrayList<>();
+		for (char[] array : result) {
+			strs.add(new String(array));
+		}
+		return strs;
+	}
 }
 
 
@@ -49,20 +49,20 @@ class Solution {
 
 //backtracking(solution)
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.length() == 0) {
-            return Collections.emptyList();
-        }
+	public List<String> letterCombinations(String digits) {
+		if (digits == null || digits.length() == 0) {
+			return Collections.emptyList();
+		}
 
-        Map<Character, char[]> mappings = new HashMap<>();
-        mappings.put('2', new char[] {'a', 'b', 'c'});
-        mappings.put('3', new char[] {'d', 'e', 'f'});
-        mappings.put('4', new char[] {'g', 'h', 'i'});
-        mappings.put('5', new char[] {'j', 'k', 'l'});
-        mappings.put('6', new char[] {'m', 'n', 'o'});
-        mappings.put('7', new char[] {'p', 'q', 'r', 's'});
-        mappings.put('8', new char[] {'t', 'u', 'v'});
-        mappings.put('9', new char[] {'w', 'x', 'y', 'z'});
+		Map<Character, char[]> mappings = new HashMap<>();
+		mappings.put('2', new char[] {'a', 'b', 'c'});
+		mappings.put('3', new char[] {'d', 'e', 'f'});
+		mappings.put('4', new char[] {'g', 'h', 'i'});
+		mappings.put('5', new char[] {'j', 'k', 'l'});
+		mappings.put('6', new char[] {'m', 'n', 'o'});
+		mappings.put('7', new char[] {'p', 'q', 'r', 's'});
+		mappings.put('8', new char[] {'t', 'u', 'v'});
+		mappings.put('9', new char[] {'w', 'x', 'y', 'z'});
 
 		List<String> result = new ArrayList<>();
 		backtrack(digits, mappings, "", result);
