9a8c503009883861a2120440f42f2f6fc3d0c441
joke111 <alta.second@gmail.com>
2019-04-01 17:15:57 +0900

    indent

--- leetcode/src/solved/32_LongestValidParentheses.java
+++ leetcode/src/solved/32_LongestValidParentheses.java
@@ -3,7 +3,7 @@
  */
 
 class Solution {
-    public int longestValidParentheses(String s) {
+	public int longestValidParentheses(String s) {
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
@@ -34,19 +34,19 @@ class Solution {
 				}
 			}
 		}
-		
-        int current = 0;
-        while(!stack.isEmpty()) {
-            int length = stack.pop();
-            if (length == -1) {
-                result = Math.max(result, current);
-                current = 0;
-            } else {
-                current += length;
-            }
-        }
-        return Math.max(result, current);
-    }
+
+		int current = 0;
+		while(!stack.isEmpty()) {
+			int length = stack.pop();
+			if (length == -1) {
+				result = Math.max(result, current);
+				current = 0;
+			} else {
+				current += length;
+			}
+		}
+		return Math.max(result, current);
+	}
 }
 
 
@@ -59,7 +59,7 @@ public class Solution {
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
-		
+
 		int max = 0;
 		for (int i = 0; i < s.length(); i++) {
 			for (int j = i + 2; j <= s.length(); j += 2) {
@@ -113,14 +113,12 @@ public class Solution {
 					memo[i] = length + 2;
 				} else {
 					int previousOpenBraceIndex = i - memo[i - 1] - 1;
-					if (previousOpenBraceIndex >= 0) {
-						if (s.charAt(previousOpenBraceIndex) == '(') {
-							int length = 0;
-							if (previousOpenBraceIndex - 1 >= 0) {
-								length = memo[previousOpenBraceIndex - 1];
-							}
-							memo[i] = memo[i - 1] + length + 2;
+					if (previousOpenBraceIndex >= 0 && s.charAt(previousOpenBraceIndex) == '(') {
+						int length = 0;
+						if (previousOpenBraceIndex - 1 >= 0) {
+							length = memo[previousOpenBraceIndex - 1];
 						}
+						memo[i] = memo[i - 1] + length + 2;
 					}
 				}
 				max = Math.max(max, memo[i]);
@@ -137,7 +135,7 @@ public class Solution {
 
 //Stack(solution)
 public class Solution {
-    public int longestValidParentheses(String s) {
+	public int longestValidParentheses(String s) {
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
@@ -150,7 +148,7 @@ public class Solution {
 			if (s.charAt(i) == '(') {
 				stack.push(i);
 			} else {
-				int top = stack.pop();
+				stack.pop();
 				if (stack.isEmpty()) {
 					stack.push(i);
 				}
@@ -208,9 +206,3 @@ public class Solution {
 		return max;
 	}
 }
-
-
-
-
-
-
--- leetcode/src/solved/69_Sqrt_x.java
+++ leetcode/src/solved/69_Sqrt_x.java
@@ -4,45 +4,45 @@
 
 //first answer
 class Solution {
-    public int mySqrt(int x) {
-        int i = 1;
-        while(x / i >= i) {
-            i++;
-        }
-        return i - 1;
-    }
+	public int mySqrt(int x) {
+		int i = 1;
+		while(x / i >= i) {
+			i++;
+		}
+		return i - 1;
+	}
 }
 
 //second answer
 class Solution {
-    public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
+	public int mySqrt(int x) {
+		if (x == 0) {
+			return 0;
+		}
 
-        int end = 1;
-        while(x / end >= end) {
-            end *= 2;
-        }
-        if (x / end == end) {
-            return end;
-        }
-        int start = end / 2;
+		int end = 1;
+		while(x / end >= end) {
+			end *= 2;
+		}
+		if (x / end == end) {
+			return end;
+		}
+		int start = end / 2;
 
-        while(start < end) {
-            if (end - start == 1) {
-                return start;
-            }
-            int middle = (start + end) / 2;
-            int value = x / middle;
-            if (value == middle) {
-                return middle;
-            } else if (value > middle) {
-                start = middle;
-            } else {
-                end = middle;
-            }
-        }
-        return 0;
-    }
+		while(start < end) {
+			if (end - start == 1) {
+				return start;
+			}
+			int middle = (start + end) / 2;
+			int value = x / middle;
+			if (value == middle) {
+				return middle;
+			} else if (value > middle) {
+				start = middle;
+			} else {
+				end = middle;
+			}
+		}
+		return 0;
+	}
 }
